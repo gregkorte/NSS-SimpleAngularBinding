aaf03949abb078de5f99e4a7b89dbec47ad94620
gregkorte <gryphionsroost@yahoo.com>
2014-12-05 10:34:06 -0600

    Added priorities.

--- js/main.js
+++ js/main.js
@@ -5,27 +5,49 @@
 		])
 	.controller('TodoController', function(){
 		var vm = this;
-		vm.tasks = [{priority: '1', 
-								name: '1st task', 
-								desc: '1st task desc', 
-								due: '1st due date'},
-								{priority: '3',
-								name: '3nd task',
-								desc: '3nd task desc',
-								due: '3nd due date'},
-								{priority: '2',
-								name: '2nd task',
-								desc: '2nd task desc',
-								due: '2nd due date'}]
+		vm.tasks = [
+			{
+				priority: 'High', 
+				name: '1st task', 
+				desc: '1st task desc', 
+				due: '1st due date'
+			},
+			{
+				priority: 'Medium',
+				name: '3nd task',
+				desc: '3nd task desc',
+				due: '3nd due date'
+			},
+			{
+				priority: 'Low',
+				name: '2nd task',
+				desc: '2nd task desc',
+				due: '2nd due date'
+			}
+		]
 
 		vm.addNewTask = function(){
 			vm.tasks.push(vm.newTask);
-			vm.newTask = null;
+			vm.newTask = _freshTask();
 		};
 
 		vm.removeTask = function(todo){
 			var index = vm.tasks.indexOf(todo);
 			vm.tasks.splice(index,1);
+		};
+
+		vm.newTask = _freshTask();
+
+		vm.priorityOptions = {
+			high: 'High',
+			medium: 'Medium',
+			low: 'Low'
+		};
+
+		function _freshTask(){
+			return {
+				priority: 'low'
+			}
 		};
 
 	}); 
