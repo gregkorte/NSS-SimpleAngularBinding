b7a34e9697b3d5ef8578e69b044c0f5474dfdb26
gregkorte <gryphionsroost@yahoo.com>
2014-12-08 14:22:12 -0600

    Includes.

--- js/main.js
+++ js/main.js
@@ -3,37 +3,34 @@
 
 	angular.module('todoApp', [//For importing other mods
 		])
-	.controller('TodoController', function(){
+	.controller('TodoController', function($http){
 		var vm = this;
-		vm.tasks = [
-			{
-				priority: 'High', 
-				name: '1st task', 
-				desc: '1st task desc', 
-				due: '1st due date'
-			},
-			{
-				priority: 'Medium',
-				name: '3nd task',
-				desc: '3nd task desc',
-				due: '3nd due date'
-			},
-			{
-				priority: 'Low',
-				name: '2nd task',
-				desc: '2nd task desc',
-				due: '2nd due date'
-			}
-		]
+
+		$http.get('https://tic-tac-toe-nssgk.firebaseio.com/.json')
+		.success(function(data){
+			vm.tasks = data;
+		})
+		.error(function(err){
+			console.log(err);
+		});
 
 		vm.addNewTask = function(){
-			vm.tasks.push(vm.newTask);
-			vm.newTask = _freshTask();
+			// vm.tasks.push(vm.newTask);
+
+			$http.post('https://tic-tac-toe-nssgk.firebaseio.com/.json', vm.newTask)
+				.success(function(data){
+					vm.tasks[data.name] = vm.newTask;
+					vm.newTask = _freshTask();
+				});
 		};
 
 		vm.removeTask = function(todo){
-			var index = vm.tasks.indexOf(todo);
-			vm.tasks.splice(index,1);
+			var id = todo
+			var url = 'https://tic-tac-toe-nssgk.firebaseio.com/' + id + '.json';
+
+			$http.delete(url);
+
+			delete vm.tasks[todo];
 		};
 
 		vm.newTask = _freshTask();
@@ -50,6 +47,6 @@
 			}
 		};
 
-	}); 
+	});
 
 }());
